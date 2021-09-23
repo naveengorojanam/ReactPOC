862bff99da6a640c598c6c4a2072bc84bf0ffe66
naveengo <naveengo@NAVEENPC>
2021-09-23 21:04:28 +0530

    Add project files.

---
+++ ReactPOC16/Scripts/JS/App.jsx
@@ -0,0 +1,11 @@
+﻿function App() {
+    //const [temp, setTemp] = React.useState("hello");
+    return (
+        <div>
+            <h1>React functional component</h1>
+            <button onClick={() => { window.alert('button clicked'); }}>click for alert</button>
+        </div>
+        )
+}
+
+React.render(<App />, document.getElementById("reactdiv"));
